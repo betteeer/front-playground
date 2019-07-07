dfd3f699d11f0415ec9da961cac56622b2c81e4f
betteeer <1577699367@qq.com>
2019-07-07 22:32:11 +0800

    create

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+const h1 = document.createElement("h1")
+h1.innerHTML = "Hello, I am Lison"
+document.body.appendChild(h1)
+
+let u: number
+let n: string | null = null
+let tuple: [string, number]
+tuple = ["a", 2]
+// tuple =['a',2,3]
+interface A {
+  a: any
+}
